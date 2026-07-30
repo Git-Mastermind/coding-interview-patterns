dbff920528ef27b564f1143cbc638792b822eeb1
Git-Mastermind <eshan-jha@outlook.com>
2026-07-30 11:27:54 -0700

    IsPalindrome works for strings that are one word with no spaces

--- IsPalindrome.java
+++ IsPalindrome.java
@@ -4,13 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class IsPalindrome {
-    public void isPalindrome(String word) {
+    public boolean isPalindrome(String word) {
         String[] strArray = word.split("");
         List<String> str = new ArrayList<>(Arrays.asList(strArray));
 
         String leftPointer = str.get(0);
         String rightPointer = str.get(str.size() - 1);
+        int leftCurrentIndex = 0;
+        int rightCurrentIndex = str.size() - 1;
 
-        
+        while (leftPointer.equals(rightPointer)) {
+            if (leftCurrentIndex == str.size() - 1 && rightCurrentIndex == 0) {
+                return true;
+            }
+            leftCurrentIndex++;
+            rightCurrentIndex--;
+
+            leftPointer = str.get(leftCurrentIndex);
+            rightPointer = str.get(rightCurrentIndex);
+        }
+        return false;
+
     }
 }
